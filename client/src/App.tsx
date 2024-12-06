69a33a6247f4f06985083a499d530f3d4b6b0fde
Lilla Guo <z5437063@ad.unsw.edu.au>
2024-12-06 16:29:22 +1100

    add basic app skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 
 
-function App() {
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { themeSettings } from "./theme";
 
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className='app'>
-     
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
